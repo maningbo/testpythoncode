c5d65a48e09565ab05605a1e7f445dfae0b52c46
maningbo <maningbo@xywy.com>
2016-09-04 19:55:40 +0800

    +

---
+++ testpythoncode/test/testArray.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+'''
+Created on 2016年3月24日  下午4:59:14
+
+@author: 马宁波
+'''
+
+testarray = ['fdsfsdf']
+str = testarray[0]
+print str
---
+++ testpythoncode/test/testTryCatch.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+'''
+Created on 2016年3月29日  上午9:41:55
+
+@author: 马宁波
+'''
+
+try:
+    num = 1*int("9")
+    print num
+except Exception,e:
+    print 'fdsfsdfs'
+    print e
